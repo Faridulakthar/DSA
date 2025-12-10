b666e76589e7901ab2a9a700577c3ab3fd27cffc
faridulakthar <faridulakthar@gmail.com>
2025-12-10 12:55:12 +0530

    feat: number pattern right angle triangle

--- problems/starPattern.ts
+++ problems/starPattern.ts
@@ -1,4 +1,4 @@
-// Star pattern problem
+// Star pattern proclem
 
 // 4 x4 star pattern
 let a: number = 4;
@@ -12,12 +12,23 @@ for (let i = 0; i < a; i++) {
 }
 
 // steps to print right angled triangle star pattern
-
 let b: number = 4;
 for (let i = 0; i < b; i++) {
   let row: string = "";
   for (let j = 0; j < i + 1; j++) {
     row = row + "* ";
+  }
+
+  console.log(row);
+}
+
+
+// steps to print right angled triangle number pattern
+let c: number = 5;
+for (let i = 0; i < c; i++) {
+  let row: string = "";
+  for (let j = 0; j <= i; j++) {
+    row = row + (j + 1) + " ";
   }
 
   console.log(row);
