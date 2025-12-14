85b1d67fd5e617e57ec5af2ad25818b426b3e725
faridulakthar <faridulakthar@gmail.com>
2025-12-15 01:02:49 +0530

    feat: palindrome

---
+++ problems/palindrome.ts
@@ -0,0 +1,24 @@
+// write a. function to check whether a string is palindrome or not
+
+function isPalindrome(num: number): boolean {
+  let rev = 0;
+  const original = num;
+
+  while (num > 0) {
+    const rem = num % 10;
+    rev = 10 * rev + rem;
+    num = Math.floor(num / 10);
+  }
+
+  if (rev === original) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const number = 12421;
+
+const palindromeResult = isPalindrome(number);
+
+console.log("Is Palindrome:", palindromeResult);
