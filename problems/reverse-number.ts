6c7f9af4594ebc62c34d17e913d34e1f0176d61b
faridulakthar <faridulakthar@gmail.com>
2025-12-17 02:03:21 +0530

    feat: reverse number

---
+++ problems/reverse-number.ts
@@ -0,0 +1,21 @@
+// write a function to reverse a number
+
+function reverse(x: number): number {
+  let rev = 0;
+  const original = x;
+
+  x = Math.abs(x);
+
+  while (x > 0) {
+    const rem = x % 10;
+    rev = 10 * rev + rem;
+    x = Math.floor(x / 10);
+  }
+
+  return original < 0 ? -rev : rev;
+}
+const testNumber: number = -12345;
+
+const reversedNumber = reverse(testNumber);
+
+console.log("Reversed Number:", reversedNumber);
