b5ba302bb75d13eaa936728ca4cfcfe043f38653
faridulakthar <faridulakthar@gmail.com>
2026-01-13 01:24:44 +0530

    feat: max consecutive ones completed

---
+++ problems/14.max-consecutive-ones.ts
@@ -0,0 +1,22 @@
+// Max Consecutive Ones
+// run: npx ts-node 14.max-consecutive-ones.ts
+
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let currentCount = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == 1) {
+      currentCount++;
+    } else {
+      maxCount = Math.max(currentCount, maxCount);
+      currentCount = 0;
+    }
+  }
+
+  return Math.max(currentCount, maxCount);
+}
+
+const newArr = [1, 1, 0, 1, 1, 1];
+const maxConsecutiveOnes = findMaxConsecutiveOnes(newArr);
+console.log("Maximum consecutive ones:", maxConsecutiveOnes);
