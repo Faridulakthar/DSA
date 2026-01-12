8926ed3f46caec7e79d2c19337668f8a367d44cc
faridulakthar <faridulakthar@gmail.com>
2026-01-12 13:51:36 +0530

    feat: merge sorted array optimized way

--- problems/12.merge-sorted-array.ts
+++ problems/12.merge-sorted-array.ts
@@ -2,7 +2,6 @@
 // run: npx ts-node 12.merge-sorted-array.ts
 
 // note: this implementation uses O(m) extra space
-
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   let nums1Copy = nums1.slice(0, m);
 
@@ -20,8 +19,34 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   }
 }
 
+// Optimized in-place solution without using space complexity O(1)
+// need to run from the end of the array to avoid overwriting values in nums1, which has enough space to hold elements of nums2
+function mergeSort(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
+  let p1 = m - 1;
+  let p2 = n - 1;
+
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break;
+    }
+
+    if (p1 >= 0 && nums1[p1]! > nums2[p2]!) {
+      nums1[i] = nums1[p1]!;
+      p1--;
+    } else {
+      nums1[i] = nums2[p2]!;
+      p2--;
+    }
+  }
+}
 
 const arr1 = [1, 2, 3, 0, 0, 0];
 const arr2 = [2, 5, 6];
-merge(arr1, 3, arr2, 3);
+// merge(arr1, 3, arr2, 3);
+mergeSort(arr1, 3, arr2, 3);
 console.log("Merged array:", arr1);
