1901a87eae1493f251a4be4bc1e282607aaf5efd
faridulakthar <faridulakthar@gmail.com>
2026-01-14 02:00:05 +0530

    feat: single number completed in xor way

--- problems/16.single-number.ts
+++ problems/16.single-number.ts
@@ -2,19 +2,32 @@
 // run: npx ts-node 16.single-number.ts
 
 function singleNumber(nums: number[]) {
-//   let hash = {};
+  //   let hash = {};
 
-//   for (let i = 0; i < nums.length; i++) {
-//     if (!hash[nums[i]]!) {
-//       hash[nums[i]!] = 1;
-//     } else {
-//       hash[nums[i]]++;
-//     }
-//   }
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (!hash[nums[i]]!) {
+  //       hash[nums[i]!] = 1;
+  //     } else {
+  //       hash[nums[i]]++;
+  //     }
+  //   }
 
-//   for (let i = 0; i < nums.length; i++) {
-//     if (hash[nums[i]] == 1) {
-//       return nums[i]!;
-//     }
-//   }
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (hash[nums[i]] == 1) {
+  //       return nums[i]!;
+  //     }
+  //   }
+
+  // XOR way ^
+
+  let xor = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    xor = xor ^ nums[i]!;
+  }
+  return xor;
 }
+
+const array = [4, 1, 2, 1, 2];
+const singleNum = singleNumber(array);
+console.log("Single number is:", singleNum);
