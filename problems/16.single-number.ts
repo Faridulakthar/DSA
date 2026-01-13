a3cfc32b9bbfb7442860d2a6ae888416c8038fda
faridulakthar <faridulakthar@gmail.com>
2026-01-14 01:58:32 +0530

    feat: single number  completed

---
+++ problems/16.single-number.ts
@@ -0,0 +1,20 @@
+// Signle Number
+// run: npx ts-node 16.single-number.ts
+
+function singleNumber(nums: number[]) {
+//   let hash = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (!hash[nums[i]]!) {
+//       hash[nums[i]!] = 1;
+//     } else {
+//       hash[nums[i]]++;
+//     }
+//   }
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (hash[nums[i]] == 1) {
+//       return nums[i]!;
+//     }
+//   }
+}
