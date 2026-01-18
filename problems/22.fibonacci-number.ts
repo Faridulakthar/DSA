666aea8324981710dc66a890469941cc6d3711d1
faridulakthar <faridulakthar@gmail.com>
2026-01-19 02:06:28 +0530

    feat: fibonacci code clean

--- problems/22.fibonacci-number.ts
+++ problems/22.fibonacci-number.ts
@@ -4,9 +4,9 @@
 export {};
 
 function fib(n: number): number {
-    if (n <= 1) return n;
-    return fib(n-1) + fib(n-2)
-};
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
+}
 
 const n = 10;
 const result = fib(n);
