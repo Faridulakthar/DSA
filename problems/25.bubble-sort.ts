75b329dbfe2379f026027767815292f405ebe557
faridulakthar <faridulakthar@gmail.com>
2026-01-21 16:40:41 +0530

    feat: bubble sort improvements

--- problems/25.bubble-sort.ts
+++ problems/25.bubble-sort.ts
@@ -7,13 +7,22 @@ function bubbleSort(arr: number[]): number[] {
   const n = arr.length;
 
   for (let i = 0; i < n - 1; i++) {
+    let isSwapped = false;
+
     for (let j = 0; j < n - 1 - 1; j++) {
       if (arr[j]! > arr[j + 1]!) {
         // Swap arr[j] and arr[j + 1]
         const temp = arr[j];
         arr[j] = arr[j + 1]!;
         arr[j + 1] = temp!;
+
+        isSwapped = true;
       }
+    }
+
+    // If no two elements were swapped in the inner loop, then the array is already sorted
+    if (!isSwapped) {
+      break;
     }
   }
 
